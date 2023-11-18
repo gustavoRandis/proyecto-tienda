cb4804530e7abca3a29c8d6584becd755318d956
gustavoRandis <gusrandis2@gmail.com>
2023-11-18 12:33:06 -0300

    18/11/23
    aa

---
+++ proyecto-tienda-productos-vendedor/reglasdenegocio/Interfaces/Repositorios/IProductosRepository.cs
@@ -0,0 +1,17 @@
+﻿using reglasdenegocio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiendita.Entity.Interfaces.Repositorios
+{
+    // Esta interfaz define métodos para interactuar con productos, incluyendo obtener por ID y todos los productos.
+    public interface IProductosRepository : IUnitOfWork<Producto>
+    {
+        //Task<Producto> obtenerporId(int Id);    
+        //Task<List<Producto>> obtenerTodoslosProductos();
+    }
+}
+/*preguntarle al chango si dentro de la interfaz producto, no debriamos tener la propiedad categoria.*/
